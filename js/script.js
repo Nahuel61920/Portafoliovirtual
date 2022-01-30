6fe9e2766e0dd47bbca3b1577905b3d4fac032bd
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-01-30 00:01:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,33 @@ window.onload = () => {
 
 }
 
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    const currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 2));
+    }
+}
+
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    const scroll = document.documentElement.scrollTop;
+
+    if (scroll > 400){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 400){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
+
 const btnSwitch = document.querySelector("#switch");
 
 
@@ -92,29 +119,3 @@ const typed = new Typed('.typed', {
     contentType: 'html', 
 });
 
-document.getElementById("button-up").addEventListener("click", scrollUp);
-
-function scrollUp(){
-
-    const currentScroll = document.documentElement.scrollTop;
-
-    if (currentScroll > 0){
-        window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 4));
-    }
-}
-
-
-buttonUp = document.getElementById("button-up");
-
-window.onscroll = function(){
-
-    const scroll = document.documentElement.scrollTop;
-
-    if (scroll > 400){
-        buttonUp.style.transform = "scale(1)";
-    }else if(scroll < 400){
-        buttonUp.style.transform = "scale(0)";
-    }
-
-}
