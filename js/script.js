b7b9c46a98ae1e8e79a83c6f76a0409d64467abb
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-02-01 19:02:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
+
 menu.onclick = () => {
     navbar.classList.toggle("activar")
 }
@@ -22,33 +23,6 @@ window.onload = () => {
 
 
 
-document.getElementById("button-up").addEventListener("click", scrollUp);
-
-function scrollUp(){
-
-    const currentScroll = document.documentElement.scrollTop;
-
-    if (currentScroll > 0){
-        window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 10));
-    }
-}
-
-
-buttonUp = document.getElementById("button-up");
-
-window.onscroll = function(){
-
-    const scroll = document.documentElement.scrollTop;
-
-    if (scroll > 500){
-        buttonUp.style.transform = "scale(1)";
-    }else if(scroll < 500){
-        buttonUp.style.transform = "scale(0)";
-    }
-
-}
-
 const btnSwitch = document.querySelector("#switch");
 
 
@@ -69,6 +43,37 @@ if(localStorage.getItem("dark-mode") === "true"){
 } else {
     document.body.classList.remove("dark");
 }
+
+
+
+
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    const currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - currentScroll );
+    }
+}
+
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    const scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
+
 
 var swiper = new Swiper(".proyectos-slider", {
     grabCursor: true,
